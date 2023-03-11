6ee15007e8e0801e4975a97ae7280f73aa86d174
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2023-03-10 22:55:12 -0300

    added hands in video

--- pages/video-player/src/index.js
+++ pages/video-player/src/index.js
@@ -1,3 +1,5 @@
 import factory from "./factory.js"
+import handGestureFactory from '../../titles/src/factories/handGestureFactory.js'
 
 await factory.initialize()
+await handGestureFactory.initialize()
